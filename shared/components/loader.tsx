f15dc2451f676b5519622c8441faf1696b0a003a
ricokurnia12 <ricokurnia19@gmail.com>
2025-09-28 18:28:27 +0700

    update loader

--- shared/components/loader.tsx
+++ shared/components/loader.tsx
@@ -5,7 +5,7 @@ export default function Loader(): JSX.Element {
         <hr className="transition-all animate-changewidth duration-1000 absolute left-0 top-1/2" />
         <img
           className="animate-bounce transition-all"
-          src="/images/logo-light.svg"
+          src="/images/logo-light.png"
           alt="Harsh Logo"
         />
         <hr className="transition-all animate-changewidth duration-1000 absolute right-0 top-1/2" />
